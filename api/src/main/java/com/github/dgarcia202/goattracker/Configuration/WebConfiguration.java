73f72acf42002c662e00ea49a05f5446bb7a7649
Diego Garcia <wft2000@gmail.com>
2018-09-28 10:16:15 +0200

    enabled totally open CORS for api

---
+++ api/src/main/java/com/github/dgarcia202/goattracker/Configuration/WebConfiguration.java
@@ -0,0 +1,20 @@
+package com.github.dgarcia202.goattracker.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // TODO: Make this more restrictive.
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
