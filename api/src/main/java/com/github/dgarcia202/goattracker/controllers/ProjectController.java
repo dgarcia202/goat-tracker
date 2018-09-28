f5bbb4bbb7f4e311966c5552c1b9309ab64371cc
Diego Garcia <wft2000@gmail.com>
2018-09-28 11:36:07 +0200

    added api end point for features per project

--- api/src/main/java/com/github/dgarcia202/goattracker/controllers/ProjectController.java
+++ api/src/main/java/com/github/dgarcia202/goattracker/controllers/ProjectController.java
@@ -1,21 +1,34 @@
 package com.github.dgarcia202.goattracker.controllers;
 
+import com.github.dgarcia202.goattracker.entities.Feature;
 import com.github.dgarcia202.goattracker.entities.Project;
+import com.github.dgarcia202.goattracker.repositories.FeatureRepository;
 import com.github.dgarcia202.goattracker.repositories.ProjectRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class ProjectController {
 
     private ProjectRepository projectRepository;
 
-    public ProjectController(ProjectRepository projectRepository) {
+    private FeatureRepository featureRepository;
+
+    public ProjectController(ProjectRepository projectRepository, FeatureRepository featureRepository) {
         this.projectRepository = projectRepository;
+        this.featureRepository = featureRepository;
     }
 
     @GetMapping("/projects")
     public Iterable<Project> getProjects() {
         return projectRepository.findAll();
+    }
+
+    @GetMapping("/projects/{projectId}/features")
+    public Iterable<Feature> getProjectFeatures(@PathVariable UUID projectId) {
+        return featureRepository.findByProjectId(projectId);
     }
 }
--- api/src/main/java/com/github/dgarcia202/goattracker/repositories/FeatureRepository.java
+++ api/src/main/java/com/github/dgarcia202/goattracker/repositories/FeatureRepository.java
@@ -1,11 +1,17 @@
 package com.github.dgarcia202.goattracker.repositories;
 
 import com.github.dgarcia202.goattracker.entities.Feature;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface FeatureRepository extends CrudRepository<Feature, UUID> {
+
+    @Query("SELECT f FROM Feature f WHERE projectId = :projectId")
+    List<Feature> findByProjectId(@Param("projectId") UUID projectId);
 }
