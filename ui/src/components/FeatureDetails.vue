3fad1f5198a3055cedd324ccc153c93ce42ddee9
Diego Garcia <wft2000@gmail.com>
2018-09-30 09:05:15 +0200

    spli main feature form in a separate component

--- ui/src/components/FeatureDetails.vue
+++ ui/src/components/FeatureDetails.vue
@@ -19,31 +19,7 @@
         <v-tabs-items v-model="selectedTab">
 
             <v-tab-item key="Feature">
-                <v-form v-model="valid" class="mx-4">
-                    <v-container grid-list-md text-xs-center fluid>
-                        <v-layout row wrap>
-                            <v-flex xs2>
-                                <v-text-field
-                                v-model="item.code"
-                                label="code"
-                                required></v-text-field>
-                            </v-flex>
-                            <v-flex xs10>
-                                <v-text-field
-                                v-model="item.name"
-                                label="Name"
-                                required />
-                            </v-flex>
-                            <v-flex xs12>
-                                <v-textarea
-                                v-model="item.description"
-                                name="input-7-1"
-                                label="Description"
-                                ></v-textarea>
-                            </v-flex>
-                        </v-layout>
-                    </v-container>
-                </v-form>
+                <FeatureDetailsMain v-model="item" />
             </v-tab-item>
 
             <v-tab-item key="User Stories">
@@ -77,16 +53,17 @@
 <script>
 import axios from '../../node_modules/axios/dist/axios.min'
 import config from '../config/Configuration'
+import FeatureDetailsMain from './FeatureDetailsMain'
 
 export default {
     name: 'FeatureDetails',
     props: [ 'projectId', 'featureId' ],
+    components: { FeatureDetailsMain },
     data() {
         return {
             selectedTab: null,
             tabTitles: [ 'Feature', 'Users Stories', 'Relationships', 'Discussion', 'Audit' ],
             item: { code: null, name: null, description: null },
-            valid: true,
             loading: true
         };
     },
---
+++ ui/src/components/FeatureDetailsMain.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-form v-model="valid" class="mx-4">
+        <v-container grid-list-md text-xs-center fluid>
+            <v-layout row wrap>
+                <v-flex xs2>
+                    <v-text-field
+                    v-model="value.code"
+                    label="code"
+                    required></v-text-field>
+                </v-flex>
+                <v-flex xs10>
+                    <v-text-field
+                    v-model="value.name"
+                    label="Name"
+                    required />
+                </v-flex>
+                <v-flex xs12>
+                    <v-textarea
+                    v-model="value.description"
+                    name="input-7-1"
+                    label="Description"
+                    ></v-textarea>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-form>
+</template>
+<script>
+export default {
+    name: 'FeatureDetailsMain',
+    props: [ 'value' ],
+    data() {
+        return {
+            valid: true
+        };
+    }
+}
+</script>
